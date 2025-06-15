339943f8d29b295bcc75a93ced4f39b557ff447e
Louis Wagner <63748857+Dannyjelll@users.noreply.github.com>
2025-06-15 18:07:10 +0200

    feat(swagger docu): Serve Swagger page on /swagger and add detailed documentation for swagger page

--- app/backend/app.py
+++ app/backend/app.py
@@ -28,7 +28,7 @@ logger.addHandler(console_handler)
 app = Flask(__name__)
 
 SWAGGER_URL = '/swagger'
-API_URL = '/backend/swagger.yaml'  # Adjusted path
+API_URL = '/backend/swagger.yaml'
 swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL)
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
