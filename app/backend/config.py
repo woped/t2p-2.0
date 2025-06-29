89f69fd28c73857e965df6032df249e71a65b295
felixbreutmann <felixbreutmann@t-online.de>
2025-06-29 16:32:43 +0200

    fix: config.py

--- app/backend/config.py
+++ app/backend/config.py
@@ -1,11 +1,4 @@
 import os
 
-# Prüfen, ob TRANSFORMER_BASE_URL gesetzt ist, ansonsten Default setzen
-if not os.getenv("TRANSFORMER_BASE_URL"):
-    os.environ["TRANSFORMER_BASE_URL"] = "https://woped.dhbw-karlsruhe.de/pnml-bpmn-transformer"
-
-if not os.getenv("OPENAI_BASE_URL"):
-    os.environ["OPENAI_BASE_URL"] = "https://api.openai.com/v1"
-
-TRANSFORMER_BASE_URL = os.environ["TRANSFORMER_BASE_URL"]
-OPENAI_BASE_URL = os.environ["OPENAI_BASE_URL"]
+TRANSFORMER_BASE_URL = os.environ.get('TRANSFORMER_BASE_URL') or 'https://woped.dhbw-karlsruhe.de/pnml-bpmn-transformer'
+OPENAI_BASE_URL = os.environ.get('OPENAI_BASE_URL') or 'https://api.openai.com/v1'
